ce032a82ad9cff1a087b703092d5168ce129ded6
Nick Graffis <nicholasgraffis@gmail.com>
2021-08-08 14:24:59 -0700

    test

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import WindiCSS from 'vite-plugin-windicss'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    WindiCSS()
+  ],
+  build: {
+    rollupInputOptions: {
+      input: ''
+    }
+  },
+  server: {
+    open: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8888/api',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+})
